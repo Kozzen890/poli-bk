d568bef4380f3e3030b6d67693b552f0aefa2e2d
Kozzen <timotiuswinzen@gmail.com>
2023-12-27 16:02:28 +0700

    change title website

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>My Personal Website</title>
+  <title>Poliklinik BK</title>
   <!--==================== UNICONS ====================-->
   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" />
   <!--==================== SWIPER CSS ====================-->
