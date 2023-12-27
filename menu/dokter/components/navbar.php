c0a17f8762ebdeebf3e188a58f0f24843fe9411f
Kozzen <timotiuswinzen@gmail.com>
2023-12-27 14:48:13 +0700

    create login admin and doctor

--- menu/dokter/components/navbar.php
+++ menu/dokter/components/navbar.php
@@ -128,6 +128,9 @@
       </a>
     </li>
     <li class="nav-item">
+      <a href="../../logout-dokter.php" class="nav-link">Logout</a>
+    </li>
+    <li class="nav-item">
       <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
         <i class="fas fa-th-large"></i>
       </a>
